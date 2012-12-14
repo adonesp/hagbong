5cc9f1cf1881778f34fadbef76e03dba20baa6b0
Adones Pitogo <adones.pitogo@gmail.com>
2012-12-14 11:34:34 +0800

    Added  root route welcome#index

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hagbong::Application.config.secret_token = 'd0e6e102ffa9a62dd75fd4a59d3b2cdae784644c2f198a6e34d25e569129c4b95a0fac16968e862352188db9c3a9ae706f4ee50718e3a9c922d18b7530faaebe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Hagbong::Application.config.session_store :cookie_store, key: '_hagbong_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Hagbong::Application.config.session_store :active_record_store
